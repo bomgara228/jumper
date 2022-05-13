cc65369041281240d186dc7c5fc2ca2deb58344c
bomgara228 <92594974+bomgara228@users.noreply.github.com>
2022-05-13 19:36:50 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+const float WINDOW_WIDTH = 600;
+const float WINDOW_HEIGHT = 800;
+const std::string WINDOW_TITLE = "Jumper";
+const size_t FPS = 60;// size_t - полная расшифровка: unsigned short int
